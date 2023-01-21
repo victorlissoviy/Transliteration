b1759f22152395468f66fd3cf2427a4fe255e895
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2023-01-21 21:19:40 +0200

    fix tests

--- transliteration/src/test/java/transliteration/TransliterationTest.java
+++ transliteration/src/test/java/transliteration/TransliterationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransliterationTest {
-  Transliteration tr = new Transliteration();
+  final Transliteration tr = new Transliteration();
 
   @Test
   void readFileTest() {
